4f403f8c39fd7baabcbb43a6eeaa240628b42e02
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-06-12 16:01:59 +0200

    Add the card component

--- apps/titlerotator/src/app/home/home.component.ts
+++ apps/titlerotator/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
@@ -25,6 +26,8 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class HomeComponent {
   private breakpointObserver = inject(BreakpointObserver);
+
+  constructor(private store: Store) { }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
